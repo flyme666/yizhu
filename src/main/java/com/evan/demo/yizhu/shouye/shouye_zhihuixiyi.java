6329974f764a3bb7fa24da9154f2a14b7038715f
flyme666 <23433653@qq.com>
2020-03-29 14:28:36 +0800

    update shouye_xiyifang

--- src/main/java/com/evan/demo/yizhu/shouye/shouye_zhihuixiyi.java
+++ src/main/java/com/evan/demo/yizhu/shouye/shouye_zhihuixiyi.java
@@ -3,7 +3,9 @@ package com.evan.demo.yizhu.shouye;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.evan.demo.yizhu.R;
 
@@ -13,12 +15,13 @@ public class shouye_zhihuixiyi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shouye_zhihuixiyi);
         ImageButton back = (ImageButton)findViewById(R.id.xiyi_back);
+        Button btn_paidui1 = (Button)findViewById(R.id.xiyifang_paidui1);
+        Button btn_paidui2 = (Button)findViewById(R.id.xiyifang_paidui2);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 shouye_zhihuixiyi.this.finish();
             }
         });
-
     }
 }
