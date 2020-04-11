c427b98fdc4a616b92020f45d3f15b313fb4fff0
flyme666 <23433653@qq.com>
2020-04-11 14:03:50 +0800

    update shouye_xiyifang

--- src/main/java/com/evan/demo/yizhu/shouye/shouye_fangke.java
+++ src/main/java/com/evan/demo/yizhu/shouye/shouye_fangke.java
@@ -1,5 +1,6 @@
 package com.evan.demo.yizhu.shouye;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,12 @@ import com.evan.demo.yizhu.R;
 
 public class shouye_fangke extends AppCompatActivity {
     private int mPreFragmentFlag = 0;
+    private int flag1 = 1;
+    private int flag2 = 0;
+    private int flag3 = 0;
+    Button btn_all ;
+    Button btn_shengqing;
+    Button btn_yichuli ;
     Fragment[] mFragments =new Fragment[3];
     private void initFragment() {
         mFragments[0] = new fangkejilu_all();
@@ -45,14 +52,30 @@ public class shouye_fangke extends AppCompatActivity {
             }
         });
         initFragment();
-        Button btn_all = (Button)findViewById(R.id.all);
-        Button btn_shengqing = (Button)findViewById(R.id.shengqing);
-        Button btn_yichuli = (Button)findViewById(R.id.yichuli);
+        btn_all = (Button)findViewById(R.id.all);
+        btn_shengqing = (Button)findViewById(R.id.shengqing);
+        btn_yichuli = (Button)findViewById(R.id.yichuli);
         btn_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showAndHideFragment(mFragments[0], mFragments[mPreFragmentFlag]);
                 mPreFragmentFlag = 0;
+                if(flag1 == 1){
+                    btn_all.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_all.setTextColor(Color.rgb(64,147,74));
+                    flag1 = 0;
+                }
+                else {
+                    btn_all.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_on));
+                    btn_all.setTextColor(Color.rgb(255,255,255));
+                    btn_shengqing.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_shengqing.setTextColor(Color.rgb(64,147,74));
+                    btn_yichuli.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_yichuli.setTextColor(Color.rgb(64,147,74));
+                    flag1 = 1;
+                    flag2 = 0;
+                    flag3 = 0;
+                }
             }
         });
         btn_shengqing.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +83,22 @@ public class shouye_fangke extends AppCompatActivity {
             public void onClick(View view) {
                 showAndHideFragment(mFragments[1], mFragments[mPreFragmentFlag]);
                 mPreFragmentFlag = 1;
+                if(flag2 == 1){
+                    btn_shengqing.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_shengqing.setTextColor(Color.rgb(64,147,74));
+                    flag2 = 0;
+                }
+                else {
+                    btn_shengqing.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_on));
+                    btn_shengqing.setTextColor(Color.rgb(255,255,255));
+                    btn_all.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_all.setTextColor(Color.rgb(64,147,74));
+                    btn_yichuli.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_yichuli.setTextColor(Color.rgb(64,147,74));
+                    flag2 = 1;
+                    flag1 = 0;
+                    flag3 = 0;
+                }
             }
         });
         btn_yichuli.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +106,22 @@ public class shouye_fangke extends AppCompatActivity {
             public void onClick(View view) {
                 showAndHideFragment(mFragments[2], mFragments[mPreFragmentFlag]);
                 mPreFragmentFlag = 2;
+                if(flag3 == 1){
+                    btn_yichuli.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_yichuli.setTextColor(Color.rgb(64,147,74));
+                    flag3 = 0;
+                }
+                else {
+                    btn_yichuli.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_on));
+                    btn_yichuli.setTextColor(Color.rgb(255,255,255));
+                    btn_all.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_all.setTextColor(Color.rgb(64,147,74));
+                    btn_shengqing.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_green_off));
+                    btn_shengqing.setTextColor(Color.rgb(64,147,74));
+                    flag3 = 1;
+                    flag1 = 0;
+                    flag2 = 0;
+                }
             }
         });
 
